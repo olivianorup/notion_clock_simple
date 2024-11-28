b3c40b3f6d067691280cda33a3a45bb9f6372d57
Olivia <125884313+olivianorup@users.noreply.github.com>
2024-11-28 17:49:45 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,65 @@
+function getWeekNumber(date){
+    const tempDate = new Date(date.getTime());
+
+    tempDate.setDate(tempDate.getDate() - tempDate.getDay() +1);
+
+    const startOfYear = new Date(tempDate.getFullYear(), 0, 1);
+
+    const daysDifference = Math.floor((tempDate - startOfYear) / (24 * 60 * 60 * 1000));
+
+    const weekNumber = Math.ceil((daysDifference + 1) / 7);
+    
+    return weekNumber;
+}
+
+function week(){
+    var today = new Date();
+    var weekNumber = getWeekNumber(today);
+
+    document.getElementById("week").innerHTML = "Uge" + " " + weekNumber;
+}
+week();
+
+function date(){
+    var today = new Date();
+
+    var day = today.getDay();
+
+    var dayList = ["Søndag", "Mandag", "Tirsdag", "Onsdag", "Torsdag", "Fredag", "Lørdag"];
+
+    var date = today.getDate();
+    var month = today.getMonth();
+    var year = today.getFullYear();
+
+    var monthList = ["Januar", "Februar", "Marts", "April", "Maj", "Juni", "Juli", "August", "September", "Oktober", "November", "December"]
+
+    document.getElementById("date").innerHTML = dayList[day] + " " + date + ". " + monthList[month] + " " + year;
+}
+date();
+
+function clock(){
+    var today = new Date();
+    var hours = today.getHours();
+    var minutes = today.getMinutes();
+    var seconds = today.getSeconds();
+
+    hours = updateTime(hours);
+    minutes = updateTime(minutes);
+    seconds = updateTime(seconds);
+
+    document.getElementById("clock").innerHTML = "<span>"+hours+"</span>" + "<spacer>"+":"+"</spacer>" + "<span>"+minutes+"</span>" + "<spacer>"+":"+"</spacer>" + "<span>"+seconds+"</span>";
+    
+    
+    var time = setTimeout(function(){
+        clock();
+    }, 1000);
+}
+clock();
+
+function updateTime(k){
+    if(k < 10){
+        return "0" + k
+    } else {
+        return k;
+    }
+}
